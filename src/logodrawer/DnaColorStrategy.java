0514a1fb1b03cc2cd25425c7cc12ecf97b0eac00
jiserte <jiserte@unq.edu.ar>
2011-07-28 13:59:17 -0300

    Code into DnaColorStrategy Class

--- src/logodrawer/DnaColorStrategy.java
+++ src/logodrawer/DnaColorStrategy.java
@@ -1,5 +1,23 @@
 package logodrawer;
 
+import java.awt.Color;
+
 public class DnaColorStrategy {
 
+	public Color getColor(char base) {
+		
+		Color result = null;
+		
+		switch (Character.toUpperCase(base)) {
+		case 'A': result = new Color(0,128,0); break;
+		case 'C': result = new Color(0,0,255); break;
+		case 'G': result = new Color(0,0,0); break;
+		case 'T': result = new Color(255,0,0); break;
+		default: break;
+		}
+		
+		return result;
+		
+	}
+	
 }
--- src/logodrawer/LogoDrawer.java
+++ src/logodrawer/LogoDrawer.java
@@ -1,7 +1,40 @@
 package logodrawer;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+
+@SuppressWarnings("restriction")
 public abstract class LogoDrawer {
 
 	
+	@SuppressWarnings("unused")
+	private BufferedImage createImage(List<PositionValues> list, ColorStrategy color, int alphabetSize ) {
+		
+		int numberOfPositions = list.size();
+		BufferedImage bi = new BufferedImage(numberOfPositions*10,100,BufferedImage.TYPE_INT_RGB);
+		Graphics2D g = bi.createGraphics();
+		
+		
+		
+		return null;
+	}
 	
+	@SuppressWarnings("unused")
+	private void 				exportJPG							(File outfile, BufferedImage bi) throws FileNotFoundException, IOException {
+		FileOutputStream out = new FileOutputStream(outfile);
+		JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+		JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam(bi);
+		param.setQuality(1.0f, false);
+		encoder.setJPEGEncodeParam(param);
+		encoder.encode(bi);
+	}
 }
