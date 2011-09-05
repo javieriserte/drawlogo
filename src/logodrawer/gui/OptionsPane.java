e335e69b2c17ed20cecd1db403d7bb18e4d5d93a
Javier Iserte <jiserte@unq.edu.ar>
2011-09-05 20:53:26 -0300

    Changes in Optiones Pane
    Signed-off-by: Javier Iserte <jiserte@unq.edu.ar>

--- src/logodrawer/gui/OptionsPane.java
+++ src/logodrawer/gui/OptionsPane.java
@@ -1,7 +1,11 @@
 package logodrawer.gui;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -25,6 +29,10 @@ public class OptionsPane extends JPanel {
 	
 	/////////////
 	// Components
+	
+	JPanel 			leftPane;
+	JPanel			rightPane;
+	
 	JTextField 		txtLogoHeader;
 	JTextField 		txtLogoHeight;
 	JTextField 		txtRowHeight;
@@ -34,7 +42,10 @@ public class OptionsPane extends JPanel {
 	JTextField 		txtPositionsPerLine;
 	JTextField 		txtList;
 	
+	
+	
 	JButton			btDraw;
+	JButton			btLoadFile;
 	JButton			btExport;
 	
 	//////////////
@@ -50,48 +61,71 @@ public class OptionsPane extends JPanel {
 	// Private Methods
 	
 	private void CreateGUI() {
-		this.setLayout(new FlowLayout());
+		GridBagLayout layout = new GridBagLayout();
 		
-		this.add(new JLabel("Logo Header"));
-		txtLogoHeader = new JTextField("100",5);
-		this.add(txtLogoHeader);
 		
-		this.add(new JLabel("Logo Height"));
-		txtLogoHeight = new JTextField("500",5);
-		this.add(txtLogoHeight);
+		layout.rowHeights = new int[]{0,25,25,25,0};
+		layout.rowWeights = new double[]{0,1,1,1,0};
+		layout.columnWidths = new int[]{0,40,0};
+		layout.columnWeights = new double[]{0,1,0};
+		
+//		this.setLayout(layout);
+		
+		
+		
+//		this.setLayout(new BorderLayout());
+		
+		leftPane = new JPanel();
+		leftPane.setLayout(layout);
+		btDraw = new JButton("Draw Logo");
+		
+		btLoadFile = new JButton("Load a file");
+		btExport = new JButton("Export JPG fle");
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5, 5, 5, 5);
+		c.gridx = 1;
+		c.gridy = 1;
+		leftPane.add(btDraw, c);
+		
+		c.gridy = 2;
+		leftPane.add(btLoadFile, c);
 
-		this.add(new JLabel("Logo Row Height"));
-		txtRowHeight = new JTextField("700",5);
-		this.add(txtRowHeight);
-		
-		this.add(new JLabel("Right Spacer:"));
-		txtRightSpacer = new JTextField("100",5);
-		this.add(txtRightSpacer);
-		
-		this.add(new JLabel("Pos Width"));
-		txtPosWidth = new JTextField("100",5);
-		this.add(txtPosWidth);
-		
-		this.add(new JLabel("Ruler Column"));
-		txtRulerColumn = new JTextField("100",5);
-		this.add(txtRulerColumn);
-		
-		this.add(new JLabel("Positiones Per Line"));
-		txtPositionsPerLine = new JTextField("20",5);
-		this.add(txtPositionsPerLine);
-		
-		txtList = new JTextField("100",5);
+		c.gridy = 3;
+		leftPane.add(btExport, c);
 
+		this.add(leftPane);
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
+//		this.add(new JLabel("Logo Header"));
+//		txtLogoHeader = new JTextField("100",3);
+//		this.add(txtLogoHeader);
+//		
+//		this.add(new JLabel("Logo Height"));
+//		txtLogoHeight = new JTextField("500",3);
+//		this.add(txtLogoHeight);
+//
+//		this.add(new JLabel("Logo Row Height"));
+//		txtRowHeight = new JTextField("700",3);
+//		this.add(txtRowHeight);
+//		
+//		this.add(new JLabel("Right Spacer:"));
+//		txtRightSpacer = new JTextField("100",3);
+//		this.add(txtRightSpacer);
+//		
+//		this.add(new JLabel("Pos Width"));
+//		txtPosWidth = new JTextField("100",3);
+//		this.add(txtPosWidth);
+//		
+//		this.add(new JLabel("Ruler Column"));
+//		txtRulerColumn = new JTextField("100",3);
+//		this.add(txtRulerColumn);
+//		
+//		this.add(new JLabel("Positiones Per Line"));
+//		txtPositionsPerLine = new JTextField("20",3);
+//		this.add(txtPositionsPerLine);
+//		
+//		txtList = new JTextField("100",5);
 		
 	}
 	
