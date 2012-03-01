0796acc7f14872d20f1726a36d4cfb75d9e3bf0e
Javier Iserte <jiserte@unq.edu.ar>
2012-03-01 20:39:11 -0300

    Version 1.1.1

--- src/logodrawer/cli/LogoDrawerCli.java
+++ src/logodrawer/cli/LogoDrawerCli.java
@@ -18,10 +18,12 @@ import logodrawer.LogoImageLayout;
 import logodrawer.MoleculeType;
 import logodrawer.gui.LogoDrawerGui;
 
+import cmdGA.MultipleOption;
 import cmdGA.NoOption;
 import cmdGA.Parser;
 import cmdGA.SingleOption;
 import cmdGA.exceptions.IncorrectParameterTypeException;
+import cmdGA.parameterType.IntegerParameter;
 import fastaIO.FastaMultipleReader;
 import fastaIO.Pair;
 
@@ -44,10 +46,14 @@ public class LogoDrawerCli {
 		
 		SingleOption molType = new SingleOption(parser, null, "--type", MoleculeTypeParameter.getParameter());
 		
-		NoOption countGaps = new NoOption(parser, false, "--NoCountGaps");
+		NoOption countGaps = new NoOption(parser, "--NoCountGaps");
 		
-		NoOption help = new NoOption(parser,false, "--help");
+		NoOption help = new NoOption(parser, "--help");
 		
+		
+		Integer[] deflayout = new Integer[]{8, 300, 20, 45, 320, 15};
+		
+		MultipleOption layoutOpt = new MultipleOption(parser, deflayout, "--layout", ',', IntegerParameter.getParameter());
 		
 		try {
 			parser.parseEx(args);
@@ -57,7 +63,7 @@ public class LogoDrawerCli {
 		
 		if (help.isPresent()) {
 
-			System.out.println("LogoDrawer Help.\nLogoDrawer is a simple utility to draw Sequences Logos as decribes in :\nSchneider TD, Stephens RM. 1990. Sequence Logos: A New Way to Display Consensus Sequences. Nucleic Acids Res. 18:6097-6100 \n\nUsage: \n       java -jar logoDrawercli.jar\n         Goes to Graphic User Interface\n\n       java -jar logoDrawercli.jar options\n         Runs command line interpreter\n         \nOptions:\n       --infile filepath \n         Reads sequences from the specified filepath. Data is expected to be in fasta format.\n         if --infile is not present then the data is readed from standard input.\n\n       --outfile filepath \n         Specifies the destination jpg file with the image.\n\n       --type moltype\n         Specifies if sequences are from DNA or Proteins, moltype can be (without the quotes):\n         \"d\" or \"DNA\"         for DNA sequences.\n         \"p\" or \"Protein\"     for Protein Sequences.\n         If --type is not present, then the type is autodetected.\n       \n       --NoCountGaps\n         If --NoCountGaps options is present, then the gaps are ignored to estimate frequencies. This result in a higher logo in the positions with gaps.\n         By default gaps are taken into account to estimate frequencies.\n         \n       --font [font name, font style]\n         By default, verdana plain font is used to draw the logos.\n         The font name can be any font installed in your system.\n         The font style is a number that specifies the style:\n           Plain        : 0.\n           Bold         : 1.\n           Italics      : 2.\n           Bold+Italics : 3.\n         The \"[\" and \"]\" symbols must be present.\n        \n       --help\n         Shows this help.\n\nExample:\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas\n       cat nuc.fas | java -jar logoDrawerCli.jar --outfile image.jpg \n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --type protein\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --font [times new roman,3]\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --NoCountGaps\n");
+			System.out.println("LogoDrawer Help.\r\nLogoDrawer is a simple utility to draw Sequences Logos as decribes in :\r\nSchneider TD, Stephens RM. 1990. Sequence Logos: A New Way to Display Consensus Sequences. Nucleic Acids Res. 18:6097-6100 \r\n\r\nUsage: \r\n       java -jar logoDrawercli.jar\r\n         Goes to Graphic User Interface\r\n\r\n       java -jar logoDrawercli.jar options\r\n         Runs command line interpreter\r\n         \r\nOptions:\r\n       --infile filepath \r\n         Reads sequences from the specified filepath. Data is expected to be in fasta format.\r\n         if --infile is not present then the data is readed from standard input.\r\n\r\n       --outfile filepath \r\n         Specifies the destination jpg file with the image.\r\n\r\n       --type moltype\r\n         Specifies if sequences are from DNA or Proteins, moltype can be (without the quotes):\r\n         \"d\" or \"DNA\"         for DNA sequences.\r\n         \"p\" or \"Protein\"     for Protein Sequences.\r\n         If --type is not present, then the type is autodetected.\r\n       \r\n       --NoCountGaps\r\n         If --NoCountGaps options is present, then the gaps are ignored to estimate frequencies. This result in a higher logo in the positions with gaps.\r\n         By default gaps are taken into account to estimate frequencies.\r\n         \r\n       --font [font name, font style]\r\n         By default, verdana plain font is used to draw the logos.\r\n         The font name can be any font installed in your system.\r\n         The font style is a number that specifies the style:\r\n           Plain        : 0.\r\n           Bold         : 1.\r\n           Italics      : 2.\r\n           Bold+Italics : 3.\r\n         The \"[\" and \"]\" symbols must be present.\r\n\r\n       --layout LogoHeader, LogoHeight, PositionsPerLine, PosWidth, RowHeight, RulerColumn \r\n         Are integers that define the layout drawing of the Sequence Logo.\r\n         Where:\r\n           LogoHeader: is the number of pixels from the top of the row to the beginning of the Logo.\r\n           LogoHeight: is the number of pixels from of the Logo.\r\n           PositionsPerLine: is the number of positions of the sequence Logo that are shown in a single line.\r\n           PosWidth: is the number os pixels of the width of each character in the logo.\r\n           RowHeight: is the number of pixels of the entire row. It includes the Logo Header, the Sequence Logo itself and the horizontal Ruler.\r\n           RulerColumn: is the number of pixels from the left to the beginning of the Sequence Logo. This part is used to draw the Vertical Ruler.\r\n         The default values are 8, 300, 20, 45, 320, 15.\r\n\r\n       --help\r\n         Shows this help.\r\n\r\nExample:\r\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas\r\n       cat nuc.fas | java -jar logoDrawerCli.jar --outfile image.jpg \r\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --type protein\r\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --font [times new roman,3]\r\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --NoCountGaps\r\n       java -jar logoDrawerCli.jar --outfile image.jpg --infile Nuc.fas --NoCountGaps -- layout 8, 300, 20, 45, 350, 45\r\n");
 			
 			return;
 		}
@@ -104,13 +110,13 @@ public class LogoDrawerCli {
 			
 			
 			layout.setFont((Font) font.getValue());
-			layout.setLogoHeader(8);
-			layout.setLogoHeight(300);
-			layout.setPositionsPerLine(20);
-			layout.setPosWidth(45);
+			layout.setLogoHeader((Integer) layoutOpt.getValue(0));
+			layout.setLogoHeight((Integer) layoutOpt.getValue(1));
+			layout.setPositionsPerLine((Integer) layoutOpt.getValue(2));
+			layout.setPosWidth((Integer) layoutOpt.getValue(3));
 			layout.setRenderingHints(renderingHints);
-			layout.setRowHeight(320);
-			layout.setRulerColumn(15);
+			layout.setRowHeight((Integer) layoutOpt.getValue(4));
+			layout.setRulerColumn((Integer) layoutOpt.getValue(5));
 			
 			BufferedImage createImage = dld.drawLogo(s,layout,!countGaps.getValue());
 		
