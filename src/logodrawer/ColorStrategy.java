71e4bc57f21b6b17830c912c9f3f7aa4a61c634d
javier <javier@martin-FIL>
2013-04-09 12:57:49 -0300

    doc on ColorStrategyt class

--- src/logodrawer/ColorStrategy.java
+++ src/logodrawer/ColorStrategy.java
@@ -2,6 +2,12 @@ package logodrawer;
 
 import java.awt.Color;
 
+/**
+ * Abstract class for coloring schemes for sequences
+ * 
+ * @author Javier Iserte
+ *
+ */
 public abstract class ColorStrategy {
 
 	public abstract Color getColor(char c);
